26b2c60d6eb1b0dd75924cb92db09e538c3759ec
priyam <priyamshankar.5@gmail.com>
2022-06-07 02:36:37 +0530

    week 4 part a all files added completed

---
+++ week4/part a/sysproc.c
@@ -0,0 +1,127 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+// #include "proc.c"
+
+int sys_fork(void)
+{
+  return fork();
+}
+
+int sys_exit(void)
+{
+  exit();
+  return 0; // not reached
+}
+
+int sys_wait(void)
+{
+  return wait();
+}
+
+int sys_kill(void)
+{
+  int pid;
+
+  if (argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if (argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if (growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if (argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+// int sys_hello(void)
+// {
+//   return 0;   //commenting this line resolved the error
+// }            //seems like this file serves another purpose, need not to write every syscall init
+
+int sys_getppid(void)
+{
+  // return myproc()->pid;
+  /* get syscall argument */
+
+  int pid;
+  if (argint(0, &pid) < 0){
+   return -1;
+  }
+  return getppid();
+}
+
+int sys_get_siblings_info(int pid){
+  
+  return get_siblings_info(sys_getpid());
+}
+void sys_signalProcess(int pid,char type[]){
+  // return 0;
+
+  // char **type2=&type1;
+  argint(0,&pid);
+  char **type1=&type;
+  argstr(1,type1); 
+  // cprintf("%s\n",*type1);
+
+  signalProcess(pid,*type1);
+}
+
+int sys_numvp(){
+  return numvp();
+}
+
+int sys_numpp(){
+  return numpp();
+}
